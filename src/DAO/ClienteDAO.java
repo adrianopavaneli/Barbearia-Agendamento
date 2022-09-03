ed9d68e8c6db3027ef0f34ca176071a14103e9ce
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-03 00:04:10 -0300

    Implementacao UsuarioDAO e ClienteDAO

---
+++ src/DAO/ClienteDAO.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Model.Cliente;
+import Model.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Adriano Pavaneli
+ */
+public class ClienteDAO {
+    private final Connection connection;
+    
+     public ClienteDAO(Connection connection){
+        this.connection = connection;
+    }
+     
+public void insert(Cliente cliente) throws SQLException{
+                   
+        String sql = "insert into cliente(nome,endereco,cep) values(?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        
+        statement.setString(1, cliente.getNome());
+        statement.setString(2, cliente.getEndereco());
+        statement.setString(2, cliente.getCep());
+        statement.execute();           
+            
+        
+    }   
+         
+ 
+ public void update(Cliente cliente) throws SQLException{
+        String sql = "update cliente set nome = ?, endereco = ?, cep = ? where id = ? ";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, cliente.getNome());
+        statement.setString(2, cliente.getEndereco());
+        statement.setString(3, cliente.getCep());
+        
+        statement.setInt(4, cliente.getId());
+        statement.execute();
+    }
+
+public void insertOuUpdate(Cliente cliente) throws SQLException{
+         if(cliente.getId() > 0){
+             update(cliente);
+             
+         }  else{
+             insert(cliente);
+         }
+}
+
+public void delete(Cliente cliente) throws SQLException{
+        String sql = "delete from cliente where id = ? ";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, cliente.getId());
+        statement.execute();
+            
+    }
+public ArrayList<Cliente> selectAll() throws SQLException{
+         String sql = "select * from cliente";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        return pesquisa(statement);        
+        
+    }
+private ArrayList<Cliente> pesquisa(PreparedStatement statement) throws SQLException {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        statement.execute();
+        ResultSet resultSet = statement.getResultSet();
+        
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String nome = resultSet.getString("nome");
+            String endereco = resultSet.getString("endereco");
+            String cep = resultSet.getString("cep");
+            
+            Cliente clienteComDados = new Cliente(id, nome, endereco, cep);
+            clientes.add(clienteComDados);
+        }
+        return clientes;
+    }
+        
+public Cliente selectPorId(Cliente cliente) throws SQLException{
+        String sql = "select * from cliente where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, cliente.getId());
+        return pesquisa(statement).get(0);
+              
+        
+    }
+    
+}
---
+++ src/DAO/UsuarioDAO.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Didi
+ */
+public class UsuarioDAO {
+    private final Connection connection;
+    
+    public UsuarioDAO(Connection connection){
+        this.connection = connection;
+    }
+    
+    public void insert(Usuario usuario) throws SQLException{
+                   
+        String sql = "insert into usuario(usuario,senha) values(?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        
+        statement.setString(1, usuario.getUsuario());
+        statement.setString(2, usuario.getSenha());
+        statement.execute();           
+            
+        
+    }
+    
+    public void update(Usuario usuario) throws SQLException{
+        String sql = "update usuario set usuario = ?, senha = ? where id = ? ";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, usuario.getUsuario());
+        statement.setString(2, usuario.getSenha());
+        statement.setInt(3, usuario.getId());
+        statement.execute();
+    }
+    
+    public void insertOuUpdate(Usuario usuario) throws SQLException{
+         if(usuario.getId() > 0){
+             update(usuario);
+             
+         }  else{
+             insert(usuario);
+         }
+        
+    }
+    public void delete(Usuario usuario) throws SQLException{
+        String sql = "delete from usuario where id = ? ";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, usuario.getId());
+        statement.execute();
+            
+    }
+    
+    public ArrayList<Usuario> selectAll() throws SQLException{
+         String sql = "select * from usuario";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        return pesquisa(statement);        
+        
+    }
+
+    private ArrayList<Usuario> pesquisa(PreparedStatement statement) throws SQLException {
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+        statement.execute();
+        ResultSet resultSet = statement.getResultSet();
+        
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String usuario = resultSet.getString("usuario");
+            String senha = resultSet.getString("senha");
+            
+            Usuario usuarioComDados = new Usuario(id, usuario, senha);
+            usuarios.add(usuarioComDados);
+        }
+        return usuarios;
+    }
+    
+    
+    public Usuario selectPorId(Usuario usuario) throws SQLException{
+        String sql = "select * from usuario where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, usuario.getId());
+        return pesquisa(statement).get(0);
+              
+        
+    }
+
+   
+
+    public boolean autenticarUsuario(Usuario usuarioNovo) throws SQLException {
+
+       String sql = "select * from usuario where usuario = ? and senha = ? ";
+       PreparedStatement statement = connection.prepareStatement(sql);
+       statement.setString(1, usuarioNovo.getUsuario());
+       statement.setString(2, usuarioNovo.getSenha());
+       statement.execute();
+       ResultSet resultset = statement.getResultSet();     
+       return resultset.next();
+    }
+    
+}
+
+    
+
